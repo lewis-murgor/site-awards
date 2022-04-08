61e45361b6fbb704d63958ebc1cb14de1533b644
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-08 16:18:55 +0300

    create models

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length =100)
+    project_image = models.ImageField(upload_to = 'images/')
+    description = HTMLField()
+    link = models.URLField(max_length =200)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'images/')
+    Bio = models.TextField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    contact = models.EmailField()
+    
