971f53fe0de519b2e54429d610853060313a5b3c
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-09 19:33:09 +0300

    create forms

--- sites/forms.py
+++ sites/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Project
+from .models import Project,Profile
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['date','user']
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
