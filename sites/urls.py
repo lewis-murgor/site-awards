2bc46320fb3c4fff2725f7dccf356004446d4af5
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-09 12:53:21 +0300

    create more urls

--- sites/urls.py
+++ sites/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$',views.index,name='index'),
-    re_path(r'^accounts/profile', views.profile, name='profile')
+    re_path(r'^accounts/profile', views.profile, name='profile'),
+    re_path(r'^project/(\d+)',views.project,name ='project'),
+    re_path(r'^post/project$', views.post_project, name='post-project'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
