e40066d941b48ee1214ddabca6f20d8c6de758f5
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-09 19:35:41 +0300

    create view functions

--- sites/views.py
+++ sites/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
-from .models import Project
+from .models import Project,Profile
 from django.contrib.auth.decorators import login_required
-from .forms import ProjectForm
+from .forms import ProjectForm,UpdateProfileForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -10,6 +10,20 @@ def index(request):
     projects = Project.objects.all()
 
     return render(request, 'index.html',{"projects":projects})
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        title = request.GET.get("project")
+        searched_projects = Project.search_by_title(title)
+        message = f"{title}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
 
 @login_required(login_url='/accounts/login/')
 def project(request,project_id):
@@ -22,9 +36,27 @@ def project(request,project_id):
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user_id = current_user)
+    profile = Profile.objects.filter(user_id = current_user)
     
 
-    return render(request, 'profile.html')
+    return render(request, 'profile.html',{"projects":projects, "profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('profile')
+
+    else:
+        form = UpdateProfileForm()
+    return render(request, 'update_profile.html', {"form": form})
 
 @login_required(login_url='/accounts/login/')
 def post_project(request):
