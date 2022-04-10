81c3f005f889074bf3cf870418dc1da6da39fa01
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-11 00:05:37 +0300

    add api urls

--- sites/urls.py
+++ sites/urls.py
@@ -10,6 +10,10 @@ urlpatterns=[
     re_path(r'^update/profile$', views.update_profile, name='update-profile'),
     re_path(r'^project/(\d+)',views.project,name ='project'),
     re_path(r'^post/project$', views.post_project, name='post-project'),
+    re_path(r'^api/profile/$', views.ProfileList.as_view()),
+    re_path(r'^api/project/$', views.ProjectList.as_view()),
+    re_path(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProfileDescription.as_view()),
+    re_path(r'api/project/project-id/(?P<pk>[0-9]+)/$',views.ProjectDescription.as_view())
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
