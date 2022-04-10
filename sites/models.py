79a232cbb21385c3968c93f2f4bc11eb0417c18f
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-11 00:03:52 +0300

    create model methods

--- sites/models.py
+++ sites/models.py
@@ -10,7 +10,20 @@ class Project(models.Model):
     description = HTMLField()
     link = models.URLField(max_length =200)
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def update_description(self, updated_description):
+        self.description = updated_description
+        self.save
 
     @classmethod
     def search_by_title(cls,title):
@@ -20,6 +33,21 @@ class Project(models.Model):
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'images/')
     Bio = models.TextField()
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     contact = models.EmailField()
+
+    def __str__(self):
+        return self.Bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self, updated_profile):
+        self.profile = updated_profile
+        self.save()
+
+
     
