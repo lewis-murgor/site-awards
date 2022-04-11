874f0a19d9aa5d11106814f9e045fb216470ad48
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-11 16:07:20 +0300

    create models

--- sites/models.py
+++ sites/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
+
+RATE_CHOICE = (
+    (10,'10'),
+    (9,'9'),
+    (8,'8'),
+    (7,'7'),
+    (6,'6'),
+    (5,'5'),
+    (4,'4'),
+    (3,'3'),
+    (2,'2'),
+    (1,'1'),
+)
 
 class Project(models.Model):
     title = models.CharField(max_length =100)
     project_image = models.ImageField(upload_to = 'images/')
-    description = HTMLField()
+    description = models.TextField()
     link = models.URLField(max_length =200)
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
@@ -48,6 +61,20 @@ class Profile(models.Model):
     def update_profile(self, updated_profile):
         self.profile = updated_profile
         self.save()
+
+class Rate(models.Model):
+    design = models.IntegerField(choices=RATE_CHOICE)
+    usability = models.IntegerField(choices=RATE_CHOICE)
+    content = models.IntegerField(choices=RATE_CHOICE)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project
+
+    def average_rating(self):
+        rating = ((self.design + self.usability + self.content)/3)
+        return rating
 
 
     
